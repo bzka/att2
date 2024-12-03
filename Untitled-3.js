c6adaf6e7094991a9c0021754e304d67d9ac5a04
bzka <ratsalt999@outlook.com>
2024-12-03 15:38:41 +0000

    Delete Untitled-3.js

--- Untitled-3.js
+++
@@ -1,6 +0,0 @@
-function continueToNextPage() {
-    window.location.href = "/Users/bzk4/downloads/html/next-page.html";
-}
-
-document.querySelector('.continue-button').addEventListener('click', continueToNextPage);
-
