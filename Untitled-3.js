aeba238ee5e8602ac89a728b2859591a9f848397
bzka <ratsalt999@outlook.com>
2024-12-03 11:27:15 +0000

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,6 @@
+function continueToNextPage() {
+    window.location.href = "/Users/bzk4/downloads/html/next-page.html";
+}
+
+document.querySelector('.continue-button').addEventListener('click', continueToNextPage);
+
